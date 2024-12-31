acd618746ad1920a3cbadb42eff7d35c452f429a
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-12-31 23:35:02 +0300

    (fix): endpoint and tests updated successfully

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     """Root endpoint of the application."""
-    name = "Emmanuel"
-
-    if name:
-        return {"message": f"Hello {name}"}
-
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    """Hello endpoint of the application."""
+    return {"message": f"Hello {name}"}
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8050)
+    uvicorn.run("app:app", host="0.0.0.0", port=8050, reload=True)
--- tests/test_root.py
+++ tests/test_root.py
@@ -12,3 +12,9 @@ class TestRootEndpoint(BaseTest):
         response = self.client.get("/non-existent-endpoint")
         self.assertEqual(response.status_code, 404)
         self.assertIn("detail", response.json())
+
+    def test_hello_endpoint(self):
+        """Test the hello endpoint of the application."""
+        response = self.client.get("/hello/Emmanuel")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello Emmanuel"})
